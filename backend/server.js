aa0a5cb4243038b67c4394f86bc27a5702d97de3
yashash ravi <yashashravi1@gmail.com>
2026-04-19 12:57:56 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+app.post('/data', (req, res) => {
+  const body = req.body;
+  res.json({ message: 'Data received', data: body });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
